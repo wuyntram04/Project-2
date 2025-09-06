b7e72e100c52ed70b6b6295436f8e596d6a36854
chikhang2303 <chikhang096@gmail.com>
2025-09-05 20:23:54 -0700

    Update Rational.h

--- Rational.h
+++ Rational.h
@@ -64,27 +64,19 @@ private:
 
 		if (numerator == 0)
 		{
-			denominator = 1;
+			return;
 		}
-		else
+		int d = gcd(numerator, denominator);
+		if (d == 0)
 		{
-			int d = gcd(numerator, denominator);
-			if (d == 0)
-			{
-				d = 1;
-			}
-
-
-			numerator = numerator / d;
-			denominator = denominator / d;
-
-			if (denominator < 0)
-			{
-				denominator = -denominator;
-				numerator = -numerator;
-			}
-
+			d = 1;
 		}
+
+
+		numerator = numerator / d;
+		denominator = denominator / d;
+
+	
 	}
 
 public:
@@ -94,9 +86,10 @@ public:
 	//postCondition: set numerator to 0 and denominator to 1
 	Rational()
 	{
-		numerator = 0;
-		denominator = 1;
+		numerator = 0.0;
+		denominator = 1.0;
 	}
+
 
 	//Constructor
 	//preCondition: n and d are integer
@@ -132,11 +125,11 @@ public:
 	//postCondition: check if the denominator is 0, set numerator as d and simlify
 	void setDenominator(int d)
 	{
+		denominator = d;
 		if (d == 0)
 		{
 			throw runtime_error("Denominator cannot be zero.");
 		}
-		denominator = d;
 		simplify();
 	}
 	
@@ -148,25 +141,52 @@ public:
 	}
 
 
-
-	// Overload operator> to compare Rational objects
-	bool operator>(const Rational& obj) const {
-		return (numerator * obj.denominator) > (obj.numerator * denominator);
-	} 
-
-	// Overload operator< for sorting (needed for std::sort)
-	bool operator<(const Rational& obj) const {
-		return (numerator * obj.denominator) < (obj.numerator * denominator);
-	}
-
-	//preCondition: obj is a object of Rational class
-    //postCondition: check if equal
-	bool operator==(const Rational& other) const
+	friend bool operator==(const Rational& R1, const Rational& R2)
 	{
+		long long R1num = llround(R1.numerator);
+		long long R1den = llround(R1.denominator);
+		long long R2num = llround(R2.numerator);
+		long long R2den = llround(R2.denominator);
 
-		return (numerator == other.numerator) && (denominator == other.denominator);
-
+		if (R1num == R2num)
+		{
+			if (R1den == R2den)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
 	}
+	
+	
+	friend bool operator<(const Rational& R1, const Rational& R2)
+	{
+		long long R1num = llround(R1.numerator);
+		long long R1den = llround(R1.denominator);
+		long long R2num = llround(R2.numerator);
+		long long R2den = llround(R2.denominator);
+
+		long long left = R1num * R2den;
+		long long right = R2num * R1den;
+
+		if (left < right)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	
 
 	
 };
@@ -182,13 +202,17 @@ Rational operator * (const Rational& R1, const Rational& R2)
 
 Rational operator / (const Rational& R1, const Rational& R2)
 {
-	if (R2.getNumerator() == 0) {
-		throw runtime_error("Division by zero rational.");
+	if (llround(R2.getNumerator()) == 0)
+	{
+		throw std::runtime_error("Division by zero rational.");
 	}
-	Rational temp;
-	temp.setNumerator(R1.getNumerator() * R2.getDenominator());
-	temp.setDenominator(R1.getDenominator() * R2.getNumerator());
-	return temp;
+	else
+	{
+		Rational temp;
+		temp.setNumerator(R1.getNumerator() * R2.getDenominator());
+		temp.setDenominator(R1.getDenominator() * R2.getNumerator());
+		return temp;
+	}
 }
 
 Rational operator + (const Rational& R1, const Rational& R2)
@@ -206,3 +230,4 @@ Rational operator - (const Rational& R1, const Rational& R2)
 	temp.setDenominator(R1.getDenominator() * R2.getDenominator());
 	return temp;
 }
+
